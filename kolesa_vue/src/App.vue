b4fc0d610d15f65136afa0b61e05604abaa06ccc
MadinaShatayeva7 <madina_-_1993@mail.ru>
2021-10-20 00:38:16 +0600

    Все карточки рендерятся

--- kolesa_vue/src/App.vue
+++ kolesa_vue/src/App.vue
@@ -27,8 +27,8 @@
                     <a href="#" class="account">
                         <img src="./assets/rick.png" alt="Картинка Рика" class="account__image">
                         <span class="account__desc">
-                            <span class="account__name">Мортиджан</span>
-                            <span class="account__text">300 баллов</span>
+                            <p class="account__name">Мортиджан</p>
+                            <p class="account__text">300 баллов</p>
                         </span>
                     </a>
                 </div>
@@ -94,7 +94,28 @@
                     <button data-id="accessories" type="button" class="js-tabs-btn tabs__item">
                       Аксессуары</button>
                 </div>
-                <div class="js-products">
+                <div class="content">
+                  <div class="content__products">
+                    <div
+                    v-for="item in products" :key="item.id"
+                    class="content__product">
+                      <div class="card">
+                        <div class="card__image">
+                          <img :src="item.img" :alt="item.alt">
+                          <span class="card__label" v-if="item.badge">New</span>
+                        </div>
+                        <div class="card__desc">
+                          <h3 class="card__ball">{{item.price}} баллов</h3>
+                          <p class="card__name">{{item.title}}</p>
+                          <p class="card__sizes">Размеры {{item.size}}</p>
+                          <button type="button" data-type="modal"
+                          class="card__btn" @click="openModal">
+                            Заказать
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
             </section>
         </main>
@@ -102,13 +123,16 @@
             <div class="footer__left">
                 <p class="footer__copyright">© Kolesa Group</p>
                 <div class="socials">
-                    <a href="https://www.instagram.com/" rel="nofollow noopener noreferrer" target="_blank" class="socials__link">
+                    <a href="https://www.instagram.com/" rel="nofollow noopener noreferrer"
+                    target="_blank" class="socials__link">
                         <img src="./assets/Instagram.svg" alt="instagram" class="socials__icon">
                     </a>
-                    <a href="https://www.youtube.com/" rel="nofollow noopener noreferrer" target="_blank" class="socials__link">
+                    <a href="https://www.youtube.com/" rel="nofollow noopener noreferrer"
+                    target="_blank" class="socials__link">
                         <img src="./assets/YouTube.svg" alt="youtube" class="socials__icon">
                     </a>
-                    <a href="https://www.vk.com/" rel="nofollow noopener noreferrer" target="_blank" class="socials__link">
+                    <a href="https://www.vk.com/" rel="nofollow noopener noreferrer"
+                    target="_blank" class="socials__link">
                         <img src="./assets/VK.svg" alt="vkontakte" class="socials__icon">
                     </a>
                 </div>
@@ -123,36 +147,110 @@
                 </div>
             </div>
         </footer>
+        <div class="modal-main-wrap" v-if="isShowModal">
+            <div class="modal-overlay" @click="closeModal"></div>
+            <div class="modal-window-wrap">
+              <div class="modal-wrapper">
+                <button type="button" @click="closeModal"
+                class="modal-wrapper__btn"></button>
+                <div class="images">
+                    <img src="./assets/image.png" alt="Футболка Эволюционируй или сдохни">
+                    <div class="images__items">
+                        <img src="./assets/image-1.png"
+                        alt="Футболка Эволюционируй или сдохни 01" class="images__item">
+                        <img src="./assets/image-2.png"
+                        alt="Футболка Эволюционируй или сдохни 02" class="images__item active">
+                        <img src="./assets/image-3.png"
+                        alt="Футболка Эволюционируй или сдохни 03" class="images__item">
+                    </div>
+                </div>
+                <div class="modal-wrapper__desc">
+                    <h3 class="modal-wrapper__title">Футболка "Эволюционируй или сдохни"</h3>
+                    <div class="modal-wrapper__blocks scores">
+                        <div class="scores__left">
+                            <h4 class="scores__left-title">100 баллов</h4>
+                            <button type="button" class="scores__btn">Попросить 50 баллов</button>
+                        </div>
+                        <div class="scores__right">
+                            <p class="scores__title">Твой баланс:</p>
+                            <p class="scores__text">50 баллов</p>
+                        </div>
+                    </div>
+                    <div class="modal-wrapper__blocks colors">
+                        <h5 class="colors__title">Цвета:</h5>
+                        <div class="colors__items">
+                            <input type="radio" class="colors__item" id="radio-1" name="colors">
+                            <label for="radio-1" class="colors__label colors__blue">Синий</label>
+
+                            <input type="radio" class="colors__item" id="radio-2" name="colors">
+                            <label for="radio-2" class="colors__label colors__beige">Бежевый</label>
+
+                            <input type="radio" class="colors__item" id="radio-3" name="colors">
+                            <label for="radio-3" class="colors__label colors__grey">Серый</label>
+                        </div>
+                    </div>
+                    <div class="modal-wrapper__blocks size">
+                        <h5 class="size__title">Размер:</h5>
+                        <div class="size__items">
+                            <input type="radio" class="size__input" id="size-1" name="size">
+                            <label for="size-1" class="size__label">S</label>
+
+                            <input type="radio" class="size__input" id="size-2" name="size">
+                            <label for="size-2" class="size__label">M</label>
+
+                            <input type="radio" class="size__input" id="size-3" name="size">
+                            <label for="size-3" class="size__label">L</label>
+                        </div>
+                    </div>
+                    <div class="modal-wrapper__blocks">
+                        <h5 class="modal-wrapper__details">Детали:</h5>
+                        <p>Брендированная толстовка от Qazaq Republic.
+                          Материал: Хлопок 80%, Вискоза 20%</p>
+                    </div>
+                    <div class="modal-wrapper__blocks">
+                        <h5 class="modal-wrapper__question">Как выбрать размер:</h5>
+                        <p>Написать дяде Рику для уточнения.</p>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
   </div>
 </template>
 
 <script>
+import { clothes, accessories } from './mock';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      isShowModal: false,
+      currentTab: 'all',
+    };
+  },
+  computed: {
+    products() {
+      switch (this.currentTab) {
+        case 'accessories': return accessories.slice();
+        case 'clothes': return clothes.slice();
+        default: return [...accessories, ...clothes];
+      }
+    },
+  },
+  methods: {
+    openModal() {
+      this.isShowModal = true;
+    },
+    closeModal() {
+      this.isShowModal = false;
+    },
+  },
 };
 </script>
 
 <style lang="scss">
-@import './styles/vars.scss';
-@import './styles/reset.scss';
-@import './styles/layout.scss';
-@import './styles/header.scss';
-@import './styles/header-mobile.scss';
-@import './styles/search.scss';
-@import './styles/account.scss';
-@import './styles/sidebar.scss';
-@import './styles/main.scss';
-@import './styles/questions.scss';
-@import './styles/tabs.scss';
-@import './styles/cards.scss';
-@import './styles/footer.scss';
-@import './styles/socials.scss';
-@import './styles/modal-window.scss';
-@import './styles/modal-images.scss';
-@import './styles/modal-scores.scss';
-@import './styles/modal-sizes.scss';
-@import './styles/modal-colors.scss';
+@import './styles/style.scss';
 
 #app {
   font-family: "Open Sans", Helvetica, Arial, sans-serif;
